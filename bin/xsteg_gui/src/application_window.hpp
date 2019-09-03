25c4589e2a661d7c70d4e24977f0d45b69b9dede
Ian A.B. King <iansens@hotmail.com>
2019-09-04 00:14:33 +0200

    roomba

--- bin/xsteg_gui/src/application_window.hpp
+++ bin/xsteg_gui/src/application_window.hpp
@@ -2,16 +2,10 @@
 
 #include "window.hpp"
 
-enum class application_window_nav
-{
-    MAIN_MENU
-};
-
 class application_window
 {
 private:
     window* _wnd;
-    application_window_nav _nav;
 
 public:
     application_window(window*);
